737e72ec6ba00301e54e5a5dcc2e7f24dd6715d1
Mohamed Amine Boulanouar <moboulan@e2r9p13.1337.ma>
2025-03-16 16:34:26 +0000

    jakefile

--- builtins/export/export.c
+++ builtins/export/export.c
@@ -6,7 +6,7 @@
 /*   By: moboulan <moboulan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 13:38:53 by moboulan          #+#    #+#             */
-/*   Updated: 2025/03/13 19:16:13 by moboulan         ###   ########.fr       */
+/*   Updated: 2025/03/16 16:33:51 by moboulan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	export_builtin(t_command command)
 {
+
 	t_env	*node;
 	char	*arg;
 	int		ret;
 	int		i;
+	int j =0 ; 
 
 	ret = EXIT_SUCCESS;
 	i = 1;
@@ -25,7 +27,16 @@ int	export_builtin(t_command command)
 		return (export_print());
 	while (command.tokens[i] && command.tokens[i]->value)
 	{
+		while(command.tokens[i] && command.tokens[i]->value && command.tokens[i+j+1] && command.tokens[i+j+1]->value )
+		{
+			if(command.tokens[i+j+1] && !command.tokens[i]->before_space && !is_operator(command.tokens[i]) && !is_operator(command.tokens[i+j+1])) {
+				command.tokens[i]->value=ft_strjoin(command.tokens[i]->value, command.tokens[i+j+1]->value);
+				command.tokens[i]->before_space=command.tokens[i+j+1]->before_space;
+			}
+			j++;
+		}
 		arg = command.tokens[i]->value;
+		printf("--%d--%s--\n",i,arg);
 		node = ft_lstnew_env_from_str(arg);
 		if (!ft_isvalid_env_key(node->key))
 		{
@@ -34,7 +45,7 @@ int	export_builtin(t_command command)
 		}
 		else
 			export_handle_argument(node);
-		i++;
+		i += j + 1; 
 	}
 	return (ret);
 }
