1461558794551f2b688a1bf907cb2e9f207963cb
aelkadir <aelkadir@gmail.com>
2025-03-16 18:26:08 +0000

    join args in epxort builtins

--- builtins/export/export.c
+++ builtins/export/export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moboulan <moboulan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aelkadir <aelkadir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 13:38:53 by moboulan          #+#    #+#             */
-/*   Updated: 2025/03/16 16:33:51 by moboulan         ###   ########.fr       */
+/*   Updated: 2025/03/16 18:25:03 by aelkadir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	export_builtin(t_command command)
 	char	*arg;
 	int		ret;
 	int		i;
-	int j =0 ; 
+	int 	j;
 
 	ret = EXIT_SUCCESS;
 	i = 1;
@@ -27,6 +27,7 @@ int	export_builtin(t_command command)
 		return (export_print());
 	while (command.tokens[i] && command.tokens[i]->value)
 	{
+		j=0;
 		while(command.tokens[i] && command.tokens[i]->value && command.tokens[i+j+1] && command.tokens[i+j+1]->value )
 		{
 			if(command.tokens[i+j+1] && !command.tokens[i]->before_space && !is_operator(command.tokens[i]) && !is_operator(command.tokens[i+j+1])) {
@@ -36,7 +37,6 @@ int	export_builtin(t_command command)
 			j++;
 		}
 		arg = command.tokens[i]->value;
-		printf("--%d--%s--\n",i,arg);
 		node = ft_lstnew_env_from_str(arg);
 		if (!ft_isvalid_env_key(node->key))
 		{
@@ -45,7 +45,7 @@ int	export_builtin(t_command command)
 		}
 		else
 			export_handle_argument(node);
-		i += j + 1; 
+		i += j +(j==0); 
 	}
 	return (ret);
 }
