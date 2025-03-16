b0175458c80cf93aeed7bdce6eb38bf1621dbf56
elkadirianas <anaskd1337@gmail.com>
2025-03-16 23:33:22 +0000

    join juillet

--- builtins/export/export.c
+++ builtins/export/export.c
@@ -12,6 +12,20 @@
 
 #include "minishell.h"
 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moboulan <moboulan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/01 13:38:53 by moboulan          #+#    #+#             */
+/*   Updated: 2025/03/16 21:13:24 by moboulan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
 int	export_builtin(t_command command)
 {
 
@@ -27,17 +41,16 @@ int	export_builtin(t_command command)
 		return (export_print());
 	while (command.tokens[i] && command.tokens[i]->value)
 	{
-		j=0;
-		while(command.tokens[i] && command.tokens[i]->value && command.tokens[i+j+1] && command.tokens[i+j+1]->value )
+		j=1;
+		while(command.tokens[i] && command.tokens[i]->value && command.tokens[i+j] && command.tokens[i+j]->value && !command.tokens[i]->before_space )
 		{
-			if(command.tokens[i+j+1] && !command.tokens[i]->before_space && !is_operator(command.tokens[i]) && !is_operator(command.tokens[i+j+1])) {
-				command.tokens[i]->value=ft_strjoin(command.tokens[i]->value, command.tokens[i+j+1]->value);
-				command.tokens[i]->before_space=command.tokens[i+j+1]->before_space;
+			if(!is_operator(command.tokens[i]) && !is_operator(command.tokens[i+j])) {
+				command.tokens[i]->value=ft_strjoin(command.tokens[i]->value, command.tokens[i+j]->value);
+				command.tokens[i]->before_space=command.tokens[i+j]->before_space;
 			}
 			j++;
 		}
 		arg = command.tokens[i]->value;
-		printf("[%s]\n",arg);
 		node = ft_lstnew_env_from_str(arg);
 		if (!ft_isvalid_env_key(node->key))
 		{
@@ -46,7 +59,7 @@ int	export_builtin(t_command command)
 		}
 		else
 			export_handle_argument(node);
-		i += j +(j==0); 
+		i += j ; 
 	}
 	return (ret);
 }
