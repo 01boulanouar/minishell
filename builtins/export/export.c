ac8737c5b37193b7d0e0d7be1b987e5f4c23f341
aelkadir <aelkadir@gmail.com>
2025-03-18 01:34:17 +0000

    join args norm

--- builtins/export/export.c
+++ builtins/export/export.c
@@ -3,6 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
+/*   By: aelkadir <aelkadir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/01 13:38:53 by moboulan          #+#    #+#             */
+/*   Updated: 2025/03/18 01:32:21 by aelkadir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
 /*   By: moboulan <moboulan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 13:38:53 by moboulan          #+#    #+#             */
@@ -26,14 +40,34 @@
 
 #include "minishell.h"
 
+static int	join_arg(t_token **tokens)
+{
+	int	j;
+	int	i;
+
+	j = 1;
+	i = 0;
+	while (tokens[i] && tokens[i]->value && tokens[i + j] && tokens[i
+		+ j]->value && !tokens[i]->before_space)
+	{
+		if (!is_operator(tokens[i]) && !is_operator(tokens[i + j]))
+		{
+			tokens[i]->value = ft_strjoin(tokens[i]->value, tokens[i
+					+ j]->value);
+			tokens[i]->before_space = tokens[i + j]->before_space;
+		}
+		j++;
+	}
+	return (j);
+}
+
 int	export_builtin(t_command command)
 {
-
 	t_env	*node;
 	char	*arg;
 	int		ret;
 	int		i;
-	int 	j;
+	int		j;
 
 	ret = EXIT_SUCCESS;
 	i = 1;
@@ -41,15 +75,7 @@ int	export_builtin(t_command command)
 		return (export_print());
 	while (command.tokens[i] && command.tokens[i]->value)
 	{
-		j=1;
-		while(command.tokens[i] && command.tokens[i]->value && command.tokens[i+j] && command.tokens[i+j]->value && !command.tokens[i]->before_space )
-		{
-			if(!is_operator(command.tokens[i]) && !is_operator(command.tokens[i+j])) {
-				command.tokens[i]->value=ft_strjoin(command.tokens[i]->value, command.tokens[i+j]->value);
-				command.tokens[i]->before_space=command.tokens[i+j]->before_space;
-			}
-			j++;
-		}
+		j = join_arg(&command.tokens[i]);
 		arg = command.tokens[i]->value;
 		node = ft_lstnew_env_from_str(arg);
 		if (!ft_isvalid_env_key(node->key))
@@ -59,7 +85,7 @@ int	export_builtin(t_command command)
 		}
 		else
 			export_handle_argument(node);
-		i += j ; 
+		i += j;
 	}
 	return (ret);
 }
